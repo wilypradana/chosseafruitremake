8965bf36c6bd78e027f68f9348554896b5125387
wilypradana <pradanawily28@gmail.com>
2023-07-20 17:21:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Hero from "./Hero.js";
 import Navbar from "./Navbar.js";
+import "./App.css";
 import "./Handler.js";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
